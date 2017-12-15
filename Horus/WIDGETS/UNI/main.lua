e569a215ea01185394c19e4dfc155ccf68547b6b
Jochen Anglett <joa@webmotions.de>
2017-12-16 00:24:42 +0100

    added copyright

--- Horus/WIDGETS/UNI/main.lua
+++ Horus/WIDGETS/UNI/main.lua
@@ -1,25 +1,22 @@
--- #############################################################################################
--- ##                                                                                         ##
--- ## Script by Jochen Anglett                                                                ##
--- ## V 1.0, 2017/12/15                                                                       ##
--- ##                                                                                         ##
--- ## This program is free software: you can redistribute it and/or modify                    ##
--- ## it under the terms of the GNU General Public License as published by                    ##
--- ## the Free Software Foundation, either version 3 of the License, or                       ##
--- ## (at your option) any later version.                                                     ##
--- ##                                                                                         ##
--- ## This program is distributed in the hope that it will be useful,                         ##
--- ## but WITHOUT ANY WARRANTY; without even the implied warranty of                          ##
--- ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                           ##
--- ## GNU General Public License for more details.                                            ##
--- ##                                                                                         ##
--- ## A copy of the GNU General Public License is available at <http://www.gnu.org/licenses/> ##
--- ##                                                                                         ##
--- ## HUD is based on Marco Ricci openxsensor hud                                             ##
--- ## Dynamic widgets is based on johfla                                                      ##                                   
--- ## Some widgets are based on work by Ollicious                                             ##                                            
--- ##                                                                                         ##
--- #############################################################################################
+-- Script by Jochen Anglett
+-- V 1.0, 2017/12/15
+-- 
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+-- 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- A copy of the GNU General Public License is available at <http://www.gnu.org/licenses/>
+--
+-- HUD is based on Marco Ricci openxsensor hud
+-- Dynamic widgets is based on johfla                                   
+-- Some widgets are based on work by Ollicious                                   
+
 
 
 local imagePath = "/WIDGETS/UNI/images/" 	-- path of widget/images on SD-Card
